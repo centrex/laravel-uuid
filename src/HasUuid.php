4d92830e44838fbbb5cb6a2610c36089b78f75ec
rochi88 <raisul.me@gmail.com>
2023-11-21 18:38:14 +0600

    .

--- src/HasUuid.php
+++ src/HasUuid.php
@@ -5,41 +5,41 @@ declare(strict_types=1);
 namespace Centrex\LaravelUuid;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
 trait HasUuid
 {
+    protected $uuid_column = config('laravel-uuid.default_uuid_column');
+
     public static function bootHasUuid(): void
     {
         static::creating(function (Model $model) {
-            if (Schema::hasColumn($model->getTable(), config('laravel-uuid.default_uuid_column'))) {
-                $model->uuid = Str::uuid()->toString();
+            if (Schema::hasColumn($model->getTable(), $model->getUuidColumnName()) && is_null($model->{$model->getUuidColumnName()})) {
+                $model->{$model->getUuidColumnName()} = Str::uuid()->toString();
             }
         });
     }
 
     /**
-     * Scope  by uuid
+     * Get the route key for the model.
      *
-     * @param  string  uuid of the model.
+     * @return string
      */
-    public function scopeUuid($query, $uuid, $first = true)
-    {
-        $match = preg_match('/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/', $uuid);
-
-        if ( ! is_string($uuid) || $match !== 1) {
-            throw (new ModelNotFoundException)->setModel(get_class($this));
-        }
-
-        $results = $query->where($this->getTable().'.'.config('laravel-uuid.default_uuid_column'), $uuid);
-
-        return $first ? $results->firstOrFail() : $results;
-    }
-
     public function getRouteKeyName()
     {
-        return 'uuid';
+        return $this->getUuidColumnName();
+    }
+
+    /** Get UUID Column Name. */
+    public function getUuidColumnName(): string
+    {
+        return isset($this->uuid_column) ? $this->uuid_column : 'uuid';
+    }
+
+    /** Scope a query to only include popular users. */
+    public function scopeUuid(Builder $query, $value): Builder
+    {
+        return $query->where($this->getUuidColumnName(), $value);
     }
 }
